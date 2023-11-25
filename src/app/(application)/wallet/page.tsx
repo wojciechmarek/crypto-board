e69d5283d818c4fde524ca28596fb3e92e354dcb
Wojciech Marek <marek.wojciech@outlook.com>
2023-11-25 13:29:35 +0100

    feat: add interactive navbar links

--- src/app/(application)/wallet/page.tsx
+++ src/app/(application)/wallet/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function WalletPage() {
-  return <main className="px-8 md:px-4">Wallelt</main>;
+  return <main className="p-4 max-w-7xl mx-auto">Wallet</main>;
 }
